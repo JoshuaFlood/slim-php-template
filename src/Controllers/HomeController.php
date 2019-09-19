8763cbb08b54678bac061527c85648398d58b12a
Joshua Flood <info@joshuaflood.com>
2019-09-19 17:09:10 +0100

    Create HomeController.php

---
+++ src/Controllers/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Controllers;
+
+use Slim\Psr7\Request;
+use Slim\Psr7\Response;
+
+class HomeController
+{
+  protected $renderer;
+
+  public function __construct($renderer)
+  {
+    $this->renderer = $renderer;
+  }
+
+  public function view(Request $request, Response $response, array $args)
+  {
+    $this->renderer->render($response, 'index.phtml', $args);
+  }
+}
